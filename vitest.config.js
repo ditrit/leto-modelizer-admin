81c37154f9b706b6607a3ea7f826e0d5f7e15753
Vincent Moittié <moittie.vincent@gmail.com>
2023-11-17 14:43:39 +0100

    Setup configuration files

---
+++ vitest.config.js
@@ -0,0 +1,45 @@
+import { defineConfig } from 'vitest/config';
+import vue from '@vitejs/plugin-vue';
+import { quasar, transformAssetUrls } from '@quasar/vite-plugin';
+import jsconfigPaths from 'vite-jsconfig-paths';
+import path from 'path';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  test: {
+    environment: 'happy-dom',
+    setupFiles: 'tests/unit/setup-file.js',
+    include: [
+      // Matches all files with extension 'js', 'jsx', 'ts' and 'tsx'
+      'tests/unit/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}',
+    ],
+    coverage: {
+      include: ['src/**/*.js', 'src/**/*.vue'],
+      extension: ['.js', '.vue'],
+      enabled: true,
+      provider: 'v8',
+      reporter: [
+        ['lcov', { projectRoot: './src' }],
+        ['json', { file: 'coverage.json' }],
+        ['text'],
+      ],
+      perFile: true,
+      all: true,
+      reportsDirectory: './reports',
+    },
+  },
+  plugins: [
+    vue({
+      template: { transformAssetUrls },
+    }),
+    quasar({
+      sassVariables: 'src/quasar-variables.scss',
+    }),
+    jsconfigPaths(),
+  ],
+  resolve: {
+    alias: {
+      src: path.resolve(__dirname, './src'),
+    },
+  },
+});
