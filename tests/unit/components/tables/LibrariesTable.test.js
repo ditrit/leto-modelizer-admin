072d79b6dd851702f10f5da27045a34890b53483
Vincent Moittié <moittie.vincent@gmail.com>
2023-12-14 11:11:08 +0100

    Refacto all tables

--- tests/unit/components/tables/LibrariesTable.test.js
+++ tests/unit/components/tables/LibrariesTable.test.js
@@ -1,19 +1,19 @@
 import { installQuasarPlugin } from '@quasar/quasar-app-extension-testing-unit-vitest';
 import { shallowMount } from '@vue/test-utils';
 import LibrariesTable from 'src/components/tables/LibrariesTable.vue';
+import * as LibraryService from 'src/services/LibraryService';
 import { vi } from 'vitest';
 
 installQuasarPlugin();
 
-vi.mock('src/composables/LetoModelizerApi');
+vi.mock('src/services/LibraryService');
 
 describe('Test component: LibrariesTable', async () => {
   let wrapper;
-  const api = await import('src/composables/LetoModelizerApi');
-
-  api.getLibraries.mockImplementation(() => Promise.resolve({ data: { results: [] } }));
 
   beforeEach(() => {
+    LibraryService.find.mockImplementation(() => Promise.resolve({ data: { results: [] } }));
+
     wrapper = shallowMount(LibrariesTable);
   });
 
