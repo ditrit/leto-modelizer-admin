63604c6b6373fc9160b6dfaaa65bb06415879d28
Vincent Moittié <moittie.vincent@gmail.com>
2023-12-18 10:08:03 +0100

    Simplify routes

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,36 +1,49 @@
 const routes = [
   {
     path: '/',
-    component: () => import('layouts/MainLayout.vue'),
     redirect: '/users',
+  },
+  {
+    path: '/users',
+    meta: {
+      name: 'users',
+    },
+    component: () => import('layouts/MainLayout.vue'),
     children: [
-      {
-        path: '/users',
-        children: [
-          { path: '', component: () => import('pages/UsersPage.vue') },
-          { path: '/users/:id', component: () => import('pages/UserPage.vue') },
-        ],
-      },
-      {
-        path: '/user-groups',
-        children: [
-          { path: '', component: () => import('pages/UserGroupsPage.vue') },
-          { path: '/user-groups/:id', component: () => import('pages/UserGroupPage.vue') },
-        ],
-      },
-      {
-        path: '/roles',
-        children: [
-          { path: '', component: () => import('pages/RolesPage.vue') },
-        ],
-      },
-      {
-        path: '/libraries',
-        children: [
-          { path: '', component: () => import('pages/LibrariesPage.vue') },
-          { path: '/libraries/:id', component: () => import('pages/LibraryPage.vue') },
-        ],
-      },
+      { path: '', component: () => import('pages/UsersPage.vue') },
+      { path: '/users/:id', component: () => import('pages/UserPage.vue') },
+    ],
+  },
+  {
+    path: '/user-groups',
+    meta: {
+      name: 'userGroups',
+    },
+    component: () => import('layouts/MainLayout.vue'),
+    children: [
+      { path: '', component: () => import('pages/UserGroupsPage.vue') },
+      { path: '/user-groups/:id', component: () => import('pages/UserGroupPage.vue') },
+    ],
+  },
+  {
+    path: '/roles',
+    meta: {
+      name: 'roles',
+    },
+    component: () => import('layouts/MainLayout.vue'),
+    children: [
+      { path: '', component: () => import('pages/RolesPage.vue') },
+    ],
+  },
+  {
+    path: '/libraries',
+    meta: {
+      name: 'libraries',
+    },
+    component: () => import('layouts/MainLayout.vue'),
+    children: [
+      { path: '', component: () => import('pages/LibrariesPage.vue') },
+      { path: '/libraries/:id', component: () => import('pages/LibraryPage.vue') },
     ],
   },
   {
