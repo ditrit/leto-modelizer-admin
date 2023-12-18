4f36595f4e53b190981846f16a5c864d1724c133
k-y0u <pierretanghepro@gmail.com>
2023-12-18 16:30:11 +0100

    Add UsersTable to UsersPage

--- src/pages/UsersPage.vue
+++ src/pages/UsersPage.vue
@@ -1,10 +1,15 @@
 <template>
-  <q-page class="flex q-pl-md bg-grey-1">
+  <q-page class="column q-pa-md bg-grey-1">
     <h4
-      class="q-ma-none q-mt-md"
+      class="q-ma-none q-mt-md q-mb-lg"
       data-cy="page_users_title"
     >
       {{ $t('IndexPage.text.title') }}
     </h4>
+    <users-table />
   </q-page>
 </template>
+
+<script setup>
+import UsersTable from 'src/components/tables/UsersTable.vue';
+</script>
