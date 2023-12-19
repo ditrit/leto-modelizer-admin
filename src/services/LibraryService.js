c29da6fb5de998d64191880be3d2238094db7738
Vincent Moittié <moittie.vincent@gmail.com>
2023-12-19 16:43:47 +0100

    Add function create in LibraryService

--- src/services/LibraryService.js
+++ src/services/LibraryService.js
@@ -33,3 +33,15 @@ export async function remove(id) {
   return api.delete(`/api/classes/Library/${id}`, { headers: getDefaultHeaders() })
     .catch(manageError);
 }
+
+/**
+ * Create library.
+ * @param {string} url - Url of the library.
+ * @param {string} roleName - Name of the library role.
+ * @returns {Promise<object>} Promise with library object on success otherwise an error.
+ */
+export async function create(url, roleName) {
+  return api.post('/api/classes/Library', { url, roleName }, { headers: getDefaultHeaders() })
+    .then(({ data }) => data)
+    .catch(manageError);
+}
