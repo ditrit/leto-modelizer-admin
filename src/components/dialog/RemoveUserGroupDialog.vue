c99746c5a218493c05b88144032a515cba52d8f9
k-y0u <pierretanghepro@gmail.com>
2023-12-28 15:02:24 +0100

    Create RemoveUserGroupDialog

---
+++ src/components/dialog/RemoveUserGroupDialog.vue
@@ -0,0 +1,72 @@
+<template>
+  <q-dialog v-model="show">
+    <q-card>
+      <q-card-section class="flex row">
+        <span class="text-h6 text-center">
+          {{ $t('RemoveUserGroupDialog.text.title', { name: userGroup.name }) }}
+        </span>
+      </q-card-section>
+      <q-form @submit="onSubmit">
+        <q-card-section class="column flex-center">
+          {{ $t('RemoveUserGroupDialog.text.content') }}
+        </q-card-section>
+        <q-card-actions align="center">
+          <q-btn
+            v-close-popup
+            :label="$t('RemoveUserGroupDialog.text.cancel')"
+            color="negative"
+          />
+          <q-btn
+            :label="$t('RemoveUserGroupDialog.text.confirm')"
+            :loading="submitting"
+            type="submit"
+            color="positive"
+            data-cy="button_confirm"
+          >
+            <template #loading>
+              <q-spinner-hourglass />
+            </template>
+          </q-btn>
+        </q-card-actions>
+      </q-form>
+    </q-card>
+  </q-dialog>
+</template>
+
+<script setup>
+import { useDialog } from 'src/composables/Dialog';
+import { ref } from 'vue';
+import ReloadUserGroupsEvent from 'src/composables/ReloadUserGroupsEvent';
+import * as UserGroupService from 'src/services/UserGroupService';
+import { Notify } from 'quasar';
+import { useI18n } from 'vue-i18n';
+
+const { t } = useI18n();
+const submitting = ref(false);
+const userGroup = ref(null);
+const { show } = useDialog('remove-userGroup', (event) => {
+  submitting.value = false;
+  userGroup.value = event.userGroup;
+});
+
+/**
+ * Remove userGroup, send event to reload all userGroups and close dialog.
+ * @returns {Promise<void>} Promise with nothing on success.
+ */
+async function onSubmit() {
+  submitting.value = true;
+
+  await UserGroupService.remove(userGroup.value.objectId);
+
+  Notify.create({
+    type: 'positive',
+    message: t('RemoveUserGroupDialog.text.notifySuccess'),
+    html: true,
+  });
+
+  ReloadUserGroupsEvent.next();
+
+  submitting.value = false;
+  show.value = false;
+}
+</script>
--- tests/unit/pages/UserGroupsPage.test.js
+++ tests/unit/pages/UserGroupsPage.test.js
@@ -3,10 +3,12 @@ import { shallowMount } from '@vue/test-utils';
 import UserGroupsPage from 'pages/UserGroupsPage.vue';
 import { useRouter } from 'vue-router';
 import { vi } from 'vitest';
+import DialogEvent from 'src/composables/DialogEvent';
 
 installQuasarPlugin();
 
 vi.mock('vue-router');
+vi.mock('src/composables/DialogEvent');
 
 describe('Test component: UserGroupsPage', () => {
   let wrapper;
@@ -28,6 +30,19 @@ describe('Test component: UserGroupsPage', () => {
       wrapper.vm.goToUserGroup('1');
 
       expect(push).toBeCalledWith('/user-groups/1');
+    });
+  });
+
+  describe('Test function: openRemoveUserGroupDialog', () => {
+    it('should open dialog', () => {
+      DialogEvent.next.mockImplementation();
+      wrapper.vm.openRemoveUserGroupDialog('test');
+
+      expect(DialogEvent.next).toBeCalledWith({
+        key: 'remove-userGroup',
+        type: 'open',
+        userGroup: 'test',
+      });
     });
   });
 });
