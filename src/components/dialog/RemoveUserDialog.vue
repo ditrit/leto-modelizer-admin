7f5accdc1f8c2c46bbb128ed1f2fecfdd696bafc
Vincent Moittié <moittie.vincent@gmail.com>
2024-01-03 15:17:24 +0100

    Fix centering title in dialog

--- src/components/dialog/RemoveUserDialog.vue
+++ src/components/dialog/RemoveUserDialog.vue
@@ -1,8 +1,8 @@
 <template>
   <q-dialog v-model="show">
     <q-card>
-      <q-card-section class="flex row">
-        <span class="text-h6 text-center">
+      <q-card-section class="flex row justify-center">
+        <span class="text-h6">
           {{ $t('RemoveUserDialog.text.title', { name: user.name }) }}
         </span>
       </q-card-section>
