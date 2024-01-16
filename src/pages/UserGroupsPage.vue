60b4e420fca0657eb719f49744cf50ff0032318a
Justine Hell <hell.justine@gmail.com>
2024-01-16 16:16:19 +0100

    Add call and props to UserGroupsTable component in UserGroupsPage

--- src/pages/UserGroupsPage.vue
+++ src/pages/UserGroupsPage.vue
@@ -7,6 +7,7 @@
       {{ $t('UserGroupsPage.text.title') }}
     </h4>
     <user-groups-table
+      :user-groups="userGroups"
       @show="goToUserGroup"
       @remove="openRemoveUserGroupDialog"
     />
@@ -17,8 +18,18 @@
 import UserGroupsTable from 'src/components/tables/UserGroupsTable.vue';
 import { useRouter } from 'vue-router';
 import DialogEvent from 'src/composables/events/DialogEvent';
+import {
+  onMounted,
+  onUnmounted,
+  ref,
+} from 'vue';
+import * as UserGroupService from 'src/services/UserGroupService';
+import ReloadUserGroupsEvent from 'src/composables/events/ReloadUserGroupsEvent';
 
 const router = useRouter();
+const userGroups = ref([]);
+
+let reloadUserGroupsEventRef;
 
 /**
  * Go to user group page.
@@ -39,4 +50,22 @@ function openRemoveUserGroupDialog(userGroup) {
     userGroup,
   });
 }
+
+/**
+ * Get user groups.
+ * @returns {Promise<void>} Promise with nothing on success.
+ */
+async function search() {
+  return UserGroupService.find().then((data) => {
+    userGroups.value = data;
+  });
+}
+
+onMounted(async () => {
+  reloadUserGroupsEventRef = ReloadUserGroupsEvent.subscribe(search);
+  await search();
+});
+onUnmounted(() => {
+  reloadUserGroupsEventRef.unsubscribe();
+});
 </script>
