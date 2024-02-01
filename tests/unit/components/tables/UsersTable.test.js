9477d5250f19f54336e204ac1409b40bab8d12fe
Justine Hell <hell.justine@gmail.com>
2024-02-01 11:06:23 +0100

    Update UsersTable with props

--- tests/unit/components/tables/UsersTable.test.js
+++ tests/unit/components/tables/UsersTable.test.js
@@ -1,49 +1,116 @@
 import { installQuasarPlugin } from '@quasar/quasar-app-extension-testing-unit-vitest';
 import { shallowMount } from '@vue/test-utils';
 import UsersTable from 'src/components/tables/UsersTable.vue';
-import { vi } from 'vitest';
-import * as UserService from 'src/services/UserService';
-import ReloadUsersEvent from 'src/composables/events/ReloadUsersEvent';
 
 installQuasarPlugin();
 
-vi.mock('src/services/UserService');
-vi.mock('src/composables/events/ReloadUsersEvent');
-
 describe('Test component: UsersTable', () => {
   let wrapper;
-  let subscribe;
-  let unsubscribe;
-
-  UserService.find.mockImplementation(() => Promise.resolve([]));
 
   beforeEach(() => {
-    subscribe = vi.fn();
-    unsubscribe = vi.fn();
-
-    ReloadUsersEvent.subscribe.mockImplementation(() => {
-      subscribe();
-      return { unsubscribe };
-    });
-
-    wrapper = shallowMount(UsersTable);
-  });
-
-  it('should mount the component', () => {
-    expect(wrapper).not.toBeNull();
-  });
-
-  describe('Test hook function: onMounted', () => {
-    it('should subscribe DialogEvent', () => {
-      expect(subscribe).toHaveBeenCalledTimes(1);
+    wrapper = shallowMount(UsersTable, {
+      props: {
+        users: [],
+        showAction: true,
+        removeAction: false,
+      },
     });
   });
 
-  describe('Test hook function: onUnmounted', () => {
-    it('should unsubscribe DialogEvent', () => {
-      expect(unsubscribe).toHaveBeenCalledTimes(0);
-      wrapper.unmount();
-      expect(unsubscribe).toHaveBeenCalledTimes(1);
+  describe('Test computed: displayActionsColumn', () => {
+    it('should be true if showAction is true', () => {
+      expect(wrapper.vm.displayActionsColumn).toBeTruthy();
+    });
+
+    it('should be true if removeAction is true', async () => {
+      await wrapper.setProps({
+        showAction: false,
+        removeAction: true,
+      });
+
+      expect(wrapper.vm.displayActionsColumn).toBeTruthy();
+    });
+
+    it('should be false if removeAction and showAction are false', async () => {
+      await wrapper.setProps({
+        showAction: false,
+        removeAction: false,
+      });
+
+      expect(wrapper.vm.displayActionsColumn).toBeFalsy();
+    });
+  });
+
+  describe('Test computed: columns', () => {
+    it('should return array with "actions" column when displayActionsColumn is truthy', () => {
+      expect(wrapper.vm.columns).toStrictEqual([
+        {
+          name: 'name',
+          required: true,
+          label: 'Name',
+          align: 'left',
+          field: 'firstname',
+          classes: 'user-firstname',
+        },
+        {
+          name: 'userName',
+          required: true,
+          label: 'UserName',
+          align: 'left',
+          field: 'username',
+          classes: 'user-username',
+        },
+        {
+          name: 'email',
+          required: true,
+          label: 'Email',
+          align: 'left',
+          field: 'email',
+          classes: 'user-email',
+        },
+        {
+          name: 'actions',
+          required: true,
+          label: 'Actions',
+          align: 'left',
+          field: 'objectId',
+          classes: 'group-actions',
+        },
+      ]);
+    });
+
+    it('should return array without "actions" column when displayActionsColumn is falsy', async () => {
+      await wrapper.setProps({
+        showAction: false,
+        removeAction: false,
+      });
+
+      expect(wrapper.vm.columns).toStrictEqual([
+        {
+          name: 'name',
+          required: true,
+          label: 'Name',
+          align: 'left',
+          field: 'firstname',
+          classes: 'user-firstname',
+        },
+        {
+          name: 'userName',
+          required: true,
+          label: 'UserName',
+          align: 'left',
+          field: 'username',
+          classes: 'user-username',
+        },
+        {
+          name: 'email',
+          required: true,
+          label: 'Email',
+          align: 'left',
+          field: 'email',
+          classes: 'user-email',
+        },
+      ]);
     });
   });
 });
