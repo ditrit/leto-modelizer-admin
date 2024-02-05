663672588855e25bbd5fc9eec1a566b667feb251
Justine Hell <hell.justine@gmail.com>
2024-02-05 10:13:51 +0100

    Add dialog in MainLayout

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -10,6 +10,7 @@
     <remove-user-dialog />
     <remove-group-dialog />
     <detach-group-from-user-dialog />
+    <detach-user-from-group-dialog />
     <attach-group-to-user-dialog />
     <attach-user-to-group-dialog />
   </q-layout>
@@ -22,6 +23,7 @@ import RemoveLibraryDialog from 'components/dialog/RemoveLibraryDialog.vue';
 import RemoveUserDialog from 'components/dialog/RemoveUserDialog.vue';
 import RemoveGroupDialog from 'components/dialog/RemoveGroupDialog.vue';
 import DetachGroupFromUserDialog from 'components/dialog/DetachGroupFromUserDialog.vue';
+import DetachUserFromGroupDialog from 'components/dialog/DetachUserFromGroupDialog.vue';
 import AttachGroupToUserDialog from 'components/dialog/AttachGroupToUserDialog.vue';
 import AttachUserToGroupDialog from 'components/dialog/AttachUserToGroupDialog.vue';
 </script>
