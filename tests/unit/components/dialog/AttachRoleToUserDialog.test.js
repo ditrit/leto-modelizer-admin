dd24439c6d567ce42b28a522f1ec3cd62b892e9f
Justine Hell <hell.justine@gmail.com>
2024-02-20 16:00:01 +0100

    Update AttachRoleToUser dialog

--- tests/unit/components/dialog/AttachRoleToUserDialog.test.js
+++ tests/unit/components/dialog/AttachRoleToUserDialog.test.js
@@ -3,19 +3,28 @@ import { mount } from '@vue/test-utils';
 import { vi } from 'vitest';
 import AttachRoleToUserDialog from 'src/components/dialog/AttachRoleToUserDialog.vue';
 import * as RoleService from 'src/services/RoleService';
+import * as UserService from 'src/services/UserService';
 import { Notify } from 'quasar';
+import { createPinia, setActivePinia } from 'pinia';
+import { useUserStore } from 'stores/UserStore';
 
 installQuasarPlugin({
   plugins: [Notify],
 });
 
 vi.mock('src/services/RoleService');
+vi.mock('src/services/UserService');
 vi.stubGlobal('$sanitize', true);
 
 describe('Test component: AttachRoleToUserDialog', () => {
   let wrapper;
+  let store;
 
   beforeEach(() => {
+    setActivePinia(createPinia());
+    store = useUserStore();
+    store.login = 'login';
+
     RoleService.find.mockImplementation(() => Promise.resolve({ content: ['roles'] }));
 
     wrapper = mount(AttachRoleToUserDialog);
@@ -53,6 +62,19 @@ describe('Test component: AttachRoleToUserDialog', () => {
       await wrapper.vm.onSubmit();
 
       expect(Notify.create).toHaveBeenCalledWith(expect.objectContaining({ type: 'negative' }));
+    });
+
+    it('should update permissions for current user', async () => {
+      wrapper.vm.userLogin = 'userLogin';
+      store.login = 'userLogin';
+      store.permissions = null;
+
+      RoleService.associateRoleAndUser.mockImplementation(() => Promise.resolve());
+      UserService.getMyPermissions.mockImplementation(() => Promise.resolve({ action: 'ACTION', entity: 'ENTITY' }));
+
+      await wrapper.vm.onSubmit();
+
+      expect(store.permissions).toEqual({ action: 'ACTION', entity: 'ENTITY' });
     });
   });
 });
