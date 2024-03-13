a0526feca8527b6b7c0a23c7d344cfb53f91f61f
Justine Hell <hell.justine@gmail.com>
2024-03-13 12:03:35 +0100

    Add all created dialogs in MainLayout

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -9,14 +9,21 @@
     <remove-library-dialog />
     <remove-user-dialog />
     <remove-group-dialog />
+    <remove-role-dialog />
     <detach-group-from-user-dialog />
     <detach-user-from-group-dialog />
     <detach-role-from-user-dialog />
+    <detach-user-from-role-dialog />
     <detach-role-from-group-dialog />
+    <detach-group-from-role-dialog />
+    <detach-permission-from-role-dialog />
     <attach-group-to-user-dialog />
     <attach-user-to-group-dialog />
     <attach-role-to-user-dialog />
+    <attach-user-to-role-dialog />
     <attach-role-to-group-dialog />
+    <attach-group-to-role-dialog />
+    <attach-permission-to-role-dialog />
     <redirect-dialog />
   </q-layout>
 </template>
@@ -27,13 +34,20 @@ import ApplicationDrawer from 'components/drawer/ApplicationDrawer.vue';
 import RemoveLibraryDialog from 'components/dialog/RemoveLibraryDialog.vue';
 import RemoveUserDialog from 'components/dialog/RemoveUserDialog.vue';
 import RemoveGroupDialog from 'components/dialog/RemoveGroupDialog.vue';
+import RemoveRoleDialog from 'components/dialog/RemoveRoleDialog.vue';
 import DetachGroupFromUserDialog from 'components/dialog/DetachGroupFromUserDialog.vue';
 import DetachUserFromGroupDialog from 'components/dialog/DetachUserFromGroupDialog.vue';
 import DetachRoleFromUserDialog from 'components/dialog/DetachRoleFromUserDialog.vue';
+import DetachUserFromRoleDialog from 'components/dialog/DetachUserFromRoleDialog.vue';
 import DetachRoleFromGroupDialog from 'components/dialog/DetachRoleFromGroupDialog.vue';
+import DetachGroupFromRoleDialog from 'components/dialog/DetachGroupFromRoleDialog.vue';
+import DetachPermissionFromRoleDialog from 'components/dialog/DetachPermissionFromRoleDialog.vue';
 import AttachGroupToUserDialog from 'components/dialog/AttachGroupToUserDialog.vue';
 import AttachUserToGroupDialog from 'components/dialog/AttachUserToGroupDialog.vue';
 import AttachRoleToUserDialog from 'components/dialog/AttachRoleToUserDialog.vue';
+import AttachUserToRoleDialog from 'components/dialog/AttachUserToRoleDialog.vue';
 import AttachRoleToGroupDialog from 'components/dialog/AttachRoleToGroupDialog.vue';
+import AttachGroupToRoleDialog from 'components/dialog/AttachGroupToRoleDialog.vue';
+import AttachPermissionToRoleDialog from 'components/dialog/AttachPermissionToRoleDialog.vue';
 import RedirectDialog from 'components/dialog/RedirectDialog.vue';
 </script>
