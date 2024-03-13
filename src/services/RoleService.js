993f171b64626c4e51a459c48699754c46417b23
Justine Hell <hell.justine@gmail.com>
2024-03-13 11:23:58 +0100

    Add methods in RoleService

--- src/services/RoleService.js
+++ src/services/RoleService.js
@@ -12,6 +12,17 @@ export async function find() {
 }
 
 /**
+ * Get role by id.
+ * @param {string} id - Role id.
+ * @returns {Promise<object>} Return a role.
+ */
+export async function findById(id) {
+  const api = await prepareRequest();
+
+  return api.get(`/roles/${id}`).then(({ data }) => data);
+}
+
+/**
  * Get all roles of a user by its login.
  * @param {string} userLogin - User login.
  * @returns {Promise<object[]>} Return an array of roles.
@@ -85,6 +96,18 @@ export async function findByGroupId(groupId) {
 }
 
 /**
+ * Get all sub roles of a role.
+ * @param {string} roleId - Role id.
+ * @returns {Promise<object>} Promise with nothing on success otherwise an error.
+ */
+export async function findSubRoles(roleId) {
+  const api = await prepareRequest();
+
+  return api.get(`/roles/${roleId}/roles`)
+    .then(({ data }) => data);
+}
+
+/**
  * Associate role and group.
  * @param {string} groupId - Group id.
  * @param {string} roleId - Role id.
@@ -110,4 +133,32 @@ export async function dissociateRoleAndGroup(groupId, roleId) {
   const api = await prepareRequest();
 
   return api.delete(`/roles/${roleId}/groups/${groupId}`);
+}
+
+/**
+ * Associate role and permission.
+ * @param {string} roleId - Role id.
+ * @param {string} permissionId - Permission id.
+ * @returns {Promise<object[]>} Return an array of permissions.
+ */
+export async function associateRoleAndPermission(roleId, permissionId) {
+  const api = await prepareRequest();
+
+  return api.post(`/roles/${roleId}/permissions`, permissionId, {
+    headers: {
+      'Content-Type': 'text/plain',
+    },
+  });
+}
+
+/**
+ * Dissociate role and permission.
+ * @param {string} roleId - Role id.
+ * @param {string} permissionId - Permission id.
+ * @returns {Promise<object>} Promise with nothing on success otherwise an error.
+ */
+export async function dissociateRoleAndPermission(roleId, permissionId) {
+  const api = await prepareRequest();
+
+  return api.delete(`/roles/${roleId}/permissions/${permissionId}`);
 }
