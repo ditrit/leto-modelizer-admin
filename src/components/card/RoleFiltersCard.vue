58c3c634e6ea5dd8bd08d22599a23fd09f7c4228
Vincent Moittié <moittie.vincent@gmail.com>
2024-03-21 15:33:26 +0100

    Add card to manage role filters

---
+++ src/components/card/RoleFiltersCard.vue
@@ -0,0 +1,31 @@
+<template>
+  <q-card>
+    <q-card-section class="row justify-center items-center">
+      <span class="text-weight-medium q-mr-md">
+        Filters
+      </span>
+      <q-input
+        outlined
+        :model-value="name"
+        label="by name"
+        debounce="2000"
+        @update:model-value="(value) => $emit('update:name', value)"
+        data-cy="role_filter_name"
+      >
+        <template #prepend>
+          <q-icon name="fa-solid fa-magnifying-glass" />
+        </template>
+      </q-input>
+    </q-card-section>
+  </q-card>
+</template>
+
+<script setup>
+defineEmits(['update:name']);
+defineProps({
+  name: {
+    type: String,
+    default: '',
+  },
+});
+</script>
