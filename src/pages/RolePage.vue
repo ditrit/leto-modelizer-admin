b463b5a3a1e29e67f7e0b2c27a11aa1ceecc42e8
Justine Hell <hell.justine@gmail.com>
2024-04-03 09:48:00 +0200

    Update RolePage with UsersTabPanel

--- src/pages/RolePage.vue
+++ src/pages/RolePage.vue
@@ -64,35 +64,11 @@
       transition-next="jump-down"
       class="bg-grey-1"
     >
-      <q-tab-panel
+      <users-tab-panel
         name="users"
-        data-cy="page_role_users_tab_panel"
-      >
-        <h6
-          class="q-ma-none q-mb-sm"
-          data-cy="page_role_users_title"
-        >
-          {{ $t('RolePage.text.userList', { role: role.name }) }}
-        </h6>
-        <q-btn
-          outline
-          no-caps
-          color="primary"
-          class="bg-white q-mb-md"
-          data-cy="page_role_button_attach_user"
-          :label="$t('RolePage.text.attachUser')"
-          :icon="$t('RolePage.icon.attach')"
-          @click="openAttachUserToRoleDialog"
-        />
-        <users-table
-          :users="users"
-          :show-action="false"
-          :remove-action="false"
-          :no-data-label="$t('UsersTable.text.noData')"
-          :no-data-icon="$t('UsersTable.icon.noData')"
-          @detach="openDetachUserFromRoleDialog"
-        />
-      </q-tab-panel>
+        type="ROLE"
+        :entity="role"
+      />
       <q-tab-panel
         name="groups"
         data-cy="page_role_groups_tab_panel"
@@ -200,29 +176,25 @@ import * as RoleService from 'src/services/RoleService';
 import { Notify } from 'quasar';
 import { useI18n } from 'vue-i18n';
 import GroupsTable from 'src/components/tables/GroupsTable.vue';
-import UsersTable from 'src/components/tables/UsersTable.vue';
 import PermissionsTable from 'src/components/tables/PermissionsTable.vue';
 import * as GroupService from 'src/services/GroupService';
-import * as UserService from 'src/services/UserService';
 import * as PermissionService from 'src/services/PermissionService';
 import DialogEvent from 'src/composables/events/DialogEvent';
 import ReloadGroupsEvent from 'src/composables/events/ReloadGroupsEvent';
-import ReloadUsersEvent from 'src/composables/events/ReloadUsersEvent';
 import ReloadPermissionsEvent from 'src/composables/events/ReloadPermissionsEvent';
 import RolesTabPanel from 'components/tab-panel/RolesTabPanel.vue';
+import UsersTabPanel from 'components/tab-panel/UsersTabPanel.vue';
 
 const loading = ref(false);
 const { t } = useI18n();
 const route = useRoute();
 const router = useRouter();
 const role = ref({});
-const users = ref([]);
 const groups = ref([]);
 const permissions = ref([]);
 const currentTab = ref('users');
 const isSuperAdmin = computed(() => role.value.name === process.env.SUPER_ADMINISTRATOR_ROLE_NAME);
 
-let reloadUsersEventRef;
 let reloadGroupsEventRef;
 let reloadPermissionsEventRef;
 
@@ -256,16 +228,6 @@ async function loadGroups() {
 }
 
 /**
- * Get all users of a role by its id.
- * @returns {Promise<void>} Promise with nothing on success.
- */
-async function loadUsers() {
-  return UserService.findByRoleId(route.params.id).then((data) => {
-    users.value = data.content;
-  });
-}
-
-/**
  * Get all permissions of a role by its id.
  * @returns {Promise<void>} Promise with nothing on success.
  */
@@ -284,23 +246,11 @@ async function search() {
   Promise.allSettled([
     loadRole(),
     loadGroups(),
-    loadUsers(),
     loadPermissions(),
   ])
     .finally(() => {
       loading.value = false;
     });
-}
-
-/**
- * Open dialog to attach a user to role.
- */
-function openAttachUserToRoleDialog() {
-  DialogEvent.next({
-    key: 'attach-user-to-role',
-    type: 'open',
-    roleId: route.params.id,
-  });
 }
 
 /**
@@ -322,19 +272,6 @@ function openAttachPermissionToRoleDialog() {
     key: 'attach-permission-to-role',
     type: 'open',
     roleId: route.params.id,
-  });
-}
-
-/**
- * Open dialog to remove role user.
- * @param {object} user - User object to remove for the dialog.
- */
-function openDetachUserFromRoleDialog(user) {
-  DialogEvent.next({
-    key: 'detach-user-from-role',
-    type: 'open',
-    user,
-    role: role.value,
   });
 }
 
@@ -366,14 +303,12 @@ function openDetachPermissionFromRoleDialog(permission) {
 
 onMounted(async () => {
   reloadGroupsEventRef = ReloadGroupsEvent.subscribe(loadGroups);
-  reloadUsersEventRef = ReloadUsersEvent.subscribe(loadUsers);
   reloadPermissionsEventRef = ReloadPermissionsEvent.subscribe(loadPermissions);
   await search();
 });
 
 onUnmounted(() => {
   reloadGroupsEventRef.unsubscribe();
-  reloadUsersEventRef.unsubscribe();
   reloadPermissionsEventRef.unsubscribe();
 });
 </script>
--- tests/unit/pages/RolePage.test.js
+++ tests/unit/pages/RolePage.test.js
@@ -4,11 +4,9 @@ import RolePage from 'pages/RolePage.vue';
 import { Notify } from 'quasar';
 import { vi } from 'vitest';
 import * as GroupService from 'src/services/GroupService';
-import * as UserService from 'src/services/UserService';
 import * as RoleService from 'src/services/RoleService';
 import * as PermissionService from 'src/services/PermissionService';
 import { useRoute, useRouter } from 'vue-router';
-import ReloadUsersEvent from 'src/composables/events/ReloadUsersEvent';
 import ReloadGroupsEvent from 'src/composables/events/ReloadGroupsEvent';
 import ReloadPermissionsEvent from 'src/composables/events/ReloadPermissionsEvent';
 import DialogEvent from 'src/composables/events/DialogEvent';
@@ -18,11 +16,9 @@ installQuasarPlugin({
 });
 
 vi.mock('src/services/GroupService');
-vi.mock('src/services/UserService');
 vi.mock('src/services/RoleService');
 vi.mock('src/services/PermissionService');
 vi.mock('vue-router');
-vi.mock('src/composables/events/ReloadUsersEvent');
 vi.mock('src/composables/events/ReloadGroupsEvent');
 vi.mock('src/composables/events/ReloadPermissionsEvent');
 vi.mock('src/composables/events/DialogEvent');
@@ -30,8 +26,6 @@ vi.mock('src/composables/events/DialogEvent');
 describe('Test component: RolePage', () => {
   let wrapper;
   let push;
-  let reloadUsersSubscribe;
-  let reloadUsersUnsubscribe;
   let reloadGroupsSubscribe;
   let reloadGroupsUnsubscribe;
   let reloadPermissionsSubscribe;
@@ -42,8 +36,6 @@ describe('Test component: RolePage', () => {
   };
 
   beforeEach(() => {
-    reloadUsersSubscribe = vi.fn();
-    reloadUsersUnsubscribe = vi.fn();
     reloadGroupsSubscribe = vi.fn();
     reloadGroupsUnsubscribe = vi.fn();
     reloadPermissionsSubscribe = vi.fn();
@@ -56,13 +48,7 @@ describe('Test component: RolePage', () => {
 
     RoleService.findById.mockImplementation(() => Promise.resolve(role));
     PermissionService.findByRoleId.mockImplementation(() => Promise.resolve({ content: 'permissions' }));
-    UserService.findByRoleId.mockImplementation(() => Promise.resolve({ content: 'users' }));
     GroupService.findByRoleId.mockImplementation(() => Promise.resolve({ content: 'groups' }));
-
-    ReloadUsersEvent.subscribe.mockImplementation(() => {
-      reloadUsersSubscribe();
-      return { unsubscribe: reloadUsersUnsubscribe };
-    });
 
     ReloadGroupsEvent.subscribe.mockImplementation(() => {
       reloadGroupsSubscribe();
@@ -105,32 +91,11 @@ describe('Test component: RolePage', () => {
     });
   });
 
-  describe('Test function: loadUsers', () => {
-    it('should set users', async () => {
-      await wrapper.vm.loadUsers();
-
-      expect(wrapper.vm.users).toEqual('users');
-    });
-  });
-
   describe('Test function: loadPermissions', () => {
     it('should set permissions', async () => {
       await wrapper.vm.loadPermissions();
 
       expect(wrapper.vm.permissions).toEqual('permissions');
-    });
-  });
-
-  describe('Test function: openAttachUserToRoleDialog', () => {
-    it('should open dialog', () => {
-      DialogEvent.next.mockImplementation();
-      wrapper.vm.openAttachUserToRoleDialog();
-
-      expect(DialogEvent.next).toBeCalledWith({
-        key: 'attach-user-to-role',
-        type: 'open',
-        roleId: 'id1',
-      });
     });
   });
 
@@ -156,20 +121,6 @@ describe('Test component: RolePage', () => {
         key: 'attach-permission-to-role',
         type: 'open',
         roleId: 'id1',
-      });
-    });
-  });
-
-  describe('Test function: openDetachUserFromRoleDialog', () => {
-    it('should open dialog', () => {
-      DialogEvent.next.mockImplementation();
-      wrapper.vm.openDetachUserFromRoleDialog('user');
-
-      expect(DialogEvent.next).toBeCalledWith({
-        key: 'detach-user-from-role',
-        type: 'open',
-        user: 'user',
-        role: { id: 1 },
       });
     });
   });
@@ -203,10 +154,6 @@ describe('Test component: RolePage', () => {
   });
 
   describe('Test hook function: onMounted', () => {
-    it('should subscribe ReloadUsersEvent', () => {
-      expect(reloadUsersSubscribe).toHaveBeenCalledTimes(1);
-    });
-
     it('should subscribe ReloadGroupsEvent', () => {
       expect(reloadGroupsSubscribe).toHaveBeenCalledTimes(1);
     });
@@ -217,12 +164,6 @@ describe('Test component: RolePage', () => {
   });
 
   describe('Test hook function: onUnmounted', () => {
-    it('should unsubscribe ReloadUsersEvent', () => {
-      expect(reloadUsersUnsubscribe).toHaveBeenCalledTimes(0);
-      wrapper.unmount();
-      expect(reloadUsersUnsubscribe).toHaveBeenCalledTimes(1);
-    });
-
     it('should unsubscribe ReloadGroupsEvent', () => {
       expect(reloadGroupsUnsubscribe).toHaveBeenCalledTimes(0);
       wrapper.unmount();
