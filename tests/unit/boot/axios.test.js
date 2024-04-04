f7094ed306f49529e6d9296c6c2cc3b4f5eebb22
Justine Hell <hell.justine@gmail.com>
2024-04-04 11:45:02 +0200

    Create method to handle filter request

--- tests/unit/boot/axios.test.js
+++ tests/unit/boot/axios.test.js
@@ -2,8 +2,20 @@ import {
   describe,
   expect,
   it,
+  vi,
 } from 'vitest';
-import { prepareQueryParameters } from 'src/boot/axios';
+import {
+  prepareQueryParameters,
+  makeFilterRequest,
+} from 'src/boot/axios';
+
+vi.mock('./useCsrfStore', () => ({
+  useCsrfStore: vi.fn(() => ({
+    headerName: '',
+    token: '',
+    expirationDate: 0,
+  })),
+}));
 
 describe('Test component: axios', () => {
   describe('Test function: prepareQueryParameters', () => {
@@ -16,6 +28,42 @@ describe('Test component: axios', () => {
     it('Should return valid string', async () => {
       expect('?name=test').toEqual(prepareQueryParameters({ name: 'test' }));
       expect('?name1=test1&name2=test2').toEqual(prepareQueryParameters({ name1: 'test1', name2: 'test2' }));
+    });
+  });
+
+  describe('Test function: makeFilterRequest', () => {
+    it('should handle pagination and make recursive requests', async () => {
+      const mockResponse1 = {
+        data: {
+          totalPages: 2,
+          pageable: {
+            pageNumber: 2,
+          },
+        },
+      };
+
+      const mockResponse2 = {
+        data: {
+          totalPages: 2,
+          pageable: {
+            pageNumber: 1,
+          },
+        },
+      };
+
+      const apiInstance = {
+        get: vi.fn()
+          .mockResolvedValueOnce(mockResponse1)
+          .mockResolvedValueOnce(mockResponse2),
+      };
+
+      const url = '/url?page=8';
+      const result = await makeFilterRequest(apiInstance, url);
+
+      expect(apiInstance.get).toHaveBeenCalledTimes(2);
+      expect(apiInstance.get).toHaveBeenNthCalledWith(1, url);
+      expect(apiInstance.get).toHaveBeenNthCalledWith(2, expect.stringMatching(/page=1/));
+      expect(result).toEqual(mockResponse2);
     });
   });
 });
