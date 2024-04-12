5327a8c9edf9b8dd035cf592707fe6d5de1c0f88
Justine Hell <hell.justine@gmail.com>
2024-04-12 11:33:08 +0200

    Create dialog to attach access control

---
+++ src/components/dialog/AttachAccessControlToAnotherDialog.vue
@@ -0,0 +1,220 @@
+<template>
+  <q-dialog v-model="show">
+    <q-card class="attach-form">
+      <q-card-section class="flex row justify-center">
+        <span class="text-h6">
+          {{ $t(`${translationKey}Dialog.text.title`) }}
+        </span>
+      </q-card-section>
+      <q-form @submit="onSubmit">
+        <q-card-section>
+          <access-control-table
+            v-model:selected="selected"
+            :access-control-type="accessControlType"
+            :rows="rows"
+            :show-action="false"
+            :remove-action="false"
+            :detach-action="false"
+            selection="multiple"
+            class="full-width"
+          />
+        </q-card-section>
+        <q-card-actions align="center">
+          <q-btn
+            v-close-popup
+            :label="$t(`${translationKey}Dialog.text.cancel`)"
+            color="negative"
+          />
+          <q-btn
+            :label="$t(`${translationKey}Dialog.text.confirm`)"
+            :loading="submitting"
+            :disable="!selected.length"
+            type="submit"
+            color="positive"
+            data-cy="button_confirm"
+          >
+            <template #loading>
+              <q-spinner-hourglass />
+            </template>
+          </q-btn>
+        </q-card-actions>
+      </q-form>
+    </q-card>
+  </q-dialog>
+</template>
+
+<script setup>
+import { useDialog } from 'src/composables/Dialog';
+import { ref, computed } from 'vue';
+import AccessControlTable from 'src/components/tables/AccessControlTable.vue';
+import ReloadGroupsEvent from 'src/composables/events/ReloadGroupsEvent';
+import ReloadRolesEvent from 'src/composables/events/ReloadRolesEvent';
+import ReloadPermissionsEvent from 'src/composables/events/ReloadPermissionsEvent';
+import SelectEvent from 'src/composables/events/SelectEvent';
+import * as GroupService from 'src/services/GroupService';
+import * as RoleService from 'src/services/RoleService';
+import * as UserService from 'src/services/UserService';
+import { Notify } from 'quasar';
+import { useI18n } from 'vue-i18n';
+import { useUserStore } from 'src/stores/UserStore';
+
+const { t } = useI18n();
+const userStore = useUserStore();
+const submitting = ref(false);
+const accessControl = ref({});
+const accessControlType = ref();
+const targetAccessControlType = ref();
+const selectOnly = ref(false);
+const selected = ref([]);
+const rows = ref([]);
+const translationKey = computed(() => {
+  if (accessControlType.value === 'role') {
+    return targetAccessControlType.value === 'role' ? 'AttachRoleToRole' : 'AttachRoleToGroup';
+  }
+
+  return targetAccessControlType.value === 'role' ? 'AttachGroupToRole' : 'AttachGroupToGroup';
+});
+
+/**
+ * Load groups and invoke the appropriate method from GroupService based on the target.
+ * @returns {object} Object that contains group filters.
+ */
+async function loadGroups() {
+  if (targetAccessControlType.value === 'group') {
+    return GroupService.find({
+      id: `not_${accessControl.value.id}`,
+    });
+  }
+
+  return GroupService.find();
+}
+
+/**
+ * Load roles and invoke the appropriate method from RoleService based on the entity value.
+ * @returns {object} Object that contains role filters.
+ */
+async function loadRoles() {
+  if (targetAccessControlType.value === 'role') {
+    return RoleService.find({
+      name: 'not_SUPER_ADMINISTRATOR',
+      id: `not_${accessControl.value.id}`,
+    });
+  }
+
+  return RoleService.find({
+    name: 'not_SUPER_ADMINISTRATOR',
+  });
+}
+
+/**
+ * Set rows value.
+ * @returns {Promise<void>} Promise with nothing on success.
+ */
+async function search() {
+  const promise = accessControlType.value === 'group' ? loadGroups : loadRoles;
+
+  return promise().then((data) => {
+    rows.value = data.content;
+  });
+}
+
+const { show } = useDialog('attach-access-control', (event) => {
+  submitting.value = false;
+  selected.value = [];
+  selectOnly.value = event.selectOnly || false;
+  accessControl.value = event.accessControl;
+  accessControlType.value = event.accessControlType;
+  targetAccessControlType.value = event.targetAccessControlType;
+
+  return search();
+});
+
+/**
+ * Attach an access control entity to another entity based on specified conditions.
+ * @param {string} accessControlIdToAttach - The ID of the access control entity to attach.
+ * @returns {Promise<any>} A promise with the result of attaching the access control entity.
+ */
+async function attachAccessControl(accessControlIdToAttach) {
+  if (accessControlType.value === 'group' && targetAccessControlType.value === 'group') {
+    return GroupService.associateGroupAndGroup(accessControlIdToAttach, accessControl.value.id);
+  }
+
+  if (accessControlType.value === 'group') {
+    return RoleService.associateRoleAndGroup(accessControlIdToAttach, accessControl.value.id);
+  }
+
+  if (targetAccessControlType.value === 'role') {
+    return RoleService.associateRoleAndRole(accessControl.value.id, accessControlIdToAttach);
+  }
+
+  return RoleService.associateRoleAndGroup(accessControl.value.id, accessControlIdToAttach);
+}
+
+/**
+ * Attach one or more groups/roles to another groups/roles.
+ * @returns {Promise<void>} Promise with nothing on success.
+ */
+async function onSubmit() {
+  if (selectOnly.value) {
+    SelectEvent.SelectRolesEvent.next(selected.value);
+    show.value = false;
+
+    return;
+  }
+
+  submitting.value = true;
+
+  const accessControlIdList = selected.value.map(({ id }) => id);
+
+  await Promise.allSettled(accessControlIdList
+    .map((accessControlId) => attachAccessControl(accessControlId)
+      .catch(() => {
+        Notify.create({
+          type: 'negative',
+          message: t(`${translationKey.value}Dialog.text.notifyError`),
+          html: true,
+        });
+
+        throw new Error(accessControlId);
+      })))
+    .then((results) => {
+      const failedRequestObjects = [];
+
+      results.forEach(({ status, reason }) => {
+        if (status === 'rejected' && reason.message) {
+          failedRequestObjects.push(...selected.value
+            .filter(({ id }) => id === reason.message));
+        }
+      });
+
+      selected.value = failedRequestObjects;
+
+      if (results.every(({ status }) => status === 'fulfilled')) {
+        Notify.create({
+          type: 'positive',
+          message: t(`${translationKey.value}Dialog.text.notifySuccess`),
+          html: true,
+        });
+
+        show.value = false;
+      }
+    })
+    .finally(async () => {
+      if (accessControlType.value === 'group') {
+        ReloadGroupsEvent.next();
+      } else {
+        ReloadRolesEvent.next();
+      }
+
+      ReloadPermissionsEvent.next();
+      userStore.permissions = await UserService.getMyPermissions();
+      submitting.value = false;
+    });
+}
+</script>
+
+<style scoped>
+.attach-form {
+  min-width: 50vw;
+}
+</style>
---
+++ tests/unit/components/dialog/AttachAccessControlToAnotherDialog.test.js
@@ -0,0 +1,247 @@
+import { installQuasarPlugin } from '@quasar/quasar-app-extension-testing-unit-vitest';
+import { mount } from '@vue/test-utils';
+import { vi } from 'vitest';
+import AttachAccessControlToAnotherDialog from 'src/components/dialog/AttachAccessControlToAnotherDialog.vue';
+import * as GroupService from 'src/services/GroupService';
+import * as RoleService from 'src/services/RoleService';
+import * as UserService from 'src/services/UserService';
+import SelectEvent from 'src/composables/events/SelectEvent';
+import ReloadGroupsEvent from 'src/composables/events/ReloadGroupsEvent';
+import ReloadRolesEvent from 'src/composables/events/ReloadRolesEvent';
+import ReloadPermissionsEvent from 'src/composables/events/ReloadPermissionsEvent';
+import { Notify } from 'quasar';
+import { createPinia, setActivePinia } from 'pinia';
+import { useUserStore } from 'stores/UserStore';
+
+installQuasarPlugin({
+  plugins: [Notify],
+});
+
+vi.mock('src/services/GroupService');
+vi.mock('src/services/RoleService');
+vi.mock('src/services/UserService');
+vi.mock('src/composables/events/SelectEvent');
+vi.mock('src/composables/events/ReloadGroupsEvent');
+vi.mock('src/composables/events/ReloadRolesEvent');
+vi.mock('src/composables/events/ReloadPermissionsEvent');
+vi.stubGlobal('$sanitize', true);
+
+describe('Test component: AttachAccessControlToAnotherDialog', () => {
+  let wrapper;
+  let store;
+
+  beforeEach(() => {
+    setActivePinia(createPinia());
+    store = useUserStore();
+
+    GroupService.find.mockImplementation(() => Promise.resolve({ content: ['groups'] }));
+    RoleService.find.mockImplementation(() => Promise.resolve({ content: ['roles'] }));
+
+    wrapper = mount(AttachAccessControlToAnotherDialog);
+  });
+
+  describe('Test computed: translationKey', () => {
+    it('should be "AttachRoleToRole"', () => {
+      store.permissions = null;
+      wrapper.vm.accessControlType = 'role';
+      wrapper.vm.targetAccessControlType = 'role';
+
+      expect(wrapper.vm.translationKey).toBe('AttachRoleToRole');
+    });
+
+    it('should be "AttachRoleToGroup"', async () => {
+      wrapper.vm.accessControlType = 'role';
+      wrapper.vm.targetAccessControlType = 'group';
+
+      expect(wrapper.vm.translationKey).toBe('AttachRoleToGroup');
+    });
+
+    it('should be "AttachGroupToRole"', async () => {
+      wrapper.vm.accessControlType = 'group';
+      wrapper.vm.targetAccessControlType = 'role';
+
+      expect(wrapper.vm.translationKey).toBe('AttachGroupToRole');
+    });
+
+    it('should be "AttachGroupToGroup"', async () => {
+      wrapper.vm.accessControlType = 'group';
+      wrapper.vm.targetAccessControlType = 'group';
+
+      expect(wrapper.vm.translationKey).toBe('AttachGroupToGroup');
+    });
+  });
+
+  describe('Test function: loadGroups', () => {
+    afterEach(() => {
+      vi.clearAllMocks();
+    });
+
+    it('should call GroupService.find with correct parameters when targetAccessControlType.value is "group"', async () => {
+      wrapper.vm.targetAccessControlType = 'group';
+      wrapper.vm.accessControl = { id: 'exampleId' };
+
+      await wrapper.vm.loadGroups();
+
+      expect(GroupService.find).toHaveBeenCalledWith({ id: 'not_exampleId' });
+    });
+
+    it('should call GroupService.find without parameters when targetAccessControlType.value is not "group"', async () => {
+      wrapper.vm.targetAccessControlType = 'not_group';
+      wrapper.vm.accessControl = { id: 'exampleId' };
+
+      await wrapper.vm.loadGroups();
+
+      expect(GroupService.find).toHaveBeenCalledWith();
+    });
+  });
+
+  describe('Test function: loadRoles', () => {
+    afterEach(() => {
+      vi.clearAllMocks();
+    });
+
+    it('should call RoleService.find with correct parameters when targetAccessControlType.value is "role"', async () => {
+      wrapper.vm.targetAccessControlType = 'role';
+      wrapper.vm.accessControl = { id: 'exampleId' };
+
+      await wrapper.vm.loadRoles();
+
+      expect(RoleService.find).toHaveBeenCalledWith({ name: 'not_SUPER_ADMINISTRATOR', id: 'not_exampleId' });
+    });
+
+    it('should call RoleService.find without parameters when targetAccessControlType.value is not "role"', async () => {
+      wrapper.vm.targetAccessControlType = 'not_role';
+      wrapper.vm.accessControl = { id: 'exampleId' };
+
+      await wrapper.vm.loadRoles();
+
+      expect(RoleService.find).toHaveBeenCalledWith({ name: 'not_SUPER_ADMINISTRATOR' });
+    });
+  });
+
+  describe('Test function: search', () => {
+    afterEach(() => {
+      vi.clearAllMocks();
+    });
+
+    it('should set rows as "groups"', async () => {
+      wrapper.vm.accessControlType = 'group';
+      wrapper.vm.rows = [];
+
+      await wrapper.vm.search();
+
+      expect(wrapper.vm.rows).toEqual(['groups']);
+    });
+
+    it('should set rows as "roles"', async () => {
+      wrapper.vm.accessControlType = 'role';
+      wrapper.vm.rows = [];
+
+      await wrapper.vm.search();
+
+      expect(wrapper.vm.rows).toEqual(['roles']);
+    });
+  });
+
+  describe('Test function: attachAccessControl', () => {
+    beforeEach(() => {
+      vi.clearAllMocks();
+      wrapper.vm.accessControl = { id: 'accessControlId' };
+      wrapper.vm.accessControlIdToAttach = 'accessControlIdToAttach';
+    });
+
+    it('should call GroupService.associateGroupAndGroup when accessControlType and targetAccessControlType are both "group"', async () => {
+      wrapper.vm.accessControlType = 'group';
+      wrapper.vm.targetAccessControlType = 'group';
+
+      await wrapper.vm.attachAccessControl('accessControlIdToAttach');
+
+      expect(GroupService.associateGroupAndGroup).toHaveBeenCalledWith('accessControlIdToAttach', 'accessControlId');
+    });
+
+    it('should call RoleService.associateRoleAndRole when accessControlType and targetAccessControlType are both "role"', async () => {
+      wrapper.vm.accessControlType = 'role';
+      wrapper.vm.targetAccessControlType = 'role';
+
+      await wrapper.vm.attachAccessControl('accessControlIdToAttach');
+
+      expect(RoleService.associateRoleAndRole).toHaveBeenCalledWith('accessControlId', 'accessControlIdToAttach');
+    });
+
+    it('should call RoleService.associateRoleAndGroup when accessControlType is "role" and targetAccessControlType is "group"', async () => {
+      wrapper.vm.accessControlType = 'role';
+      wrapper.vm.targetAccessControlType = 'group';
+
+      await wrapper.vm.attachAccessControl('accessControlIdToAttach');
+
+      expect(RoleService.associateRoleAndGroup).toHaveBeenCalledWith('accessControlId', 'accessControlIdToAttach');
+    });
+
+    it('should call RoleService.associateRoleAndGroup when accessControlType is "group" and targetAccessControlType is "role"', async () => {
+      wrapper.vm.accessControlType = 'group';
+      wrapper.vm.targetAccessControlType = 'role';
+
+      await wrapper.vm.attachAccessControl('accessControlIdToAttach');
+
+      expect(RoleService.associateRoleAndGroup).toHaveBeenCalledWith('accessControlIdToAttach', 'accessControlId');
+    });
+  });
+
+  describe('Test function: onSubmit', () => {
+    beforeEach(() => {
+      wrapper.vm.selected = [{ id: '1' }, { id: '2' }];
+      wrapper.vm.selectOnly = false;
+      wrapper.vm.accessControlType = 'group';
+      wrapper.vm.targetAccessControlType = 'group';
+      wrapper.vm.show = true;
+      wrapper.vm.submitting = false;
+      store.permissions = null;
+
+      UserService.getMyPermissions.mockImplementation(() => Promise.resolve({ action: 'ACTION', entity: 'ENTITY' }));
+    });
+
+    it('should send SelectRoles event', async () => {
+      wrapper.vm.selectOnly = true;
+      SelectEvent.SelectRolesEvent.next = vi.fn();
+
+      await wrapper.vm.onSubmit();
+
+      expect(SelectEvent.SelectRolesEvent.next).toHaveBeenCalled();
+    });
+
+    it('should handle successful submission', async () => {
+      Notify.create = vi.fn();
+
+      await wrapper.vm.onSubmit();
+
+      expect(Notify.create).toHaveBeenCalledWith({
+        type: 'positive',
+        message: 'Group(s) successfully attached to the group.',
+        html: true,
+      });
+      expect(wrapper.vm.show).toBe(false);
+      expect(store.permissions).toEqual({ action: 'ACTION', entity: 'ENTITY' });
+      expect(wrapper.vm.submitting).toBe(false);
+      expect(ReloadGroupsEvent.next).toHaveBeenCalled();
+      expect(ReloadPermissionsEvent.next).toHaveBeenCalled();
+    });
+
+    it('should handle failed submission', async () => {
+      wrapper.vm.accessControlType = 'role';
+      wrapper.vm.targetAccessControlType = 'role';
+      Notify.create = vi.fn();
+
+      RoleService.associateRoleAndRole.mockImplementation(() => Promise.reject());
+
+      await wrapper.vm.onSubmit();
+
+      expect(Notify.create).toHaveBeenCalledWith({
+        type: 'negative',
+        message: 'Error during attachment of a role to the role.',
+        html: true,
+      });
+      expect(ReloadRolesEvent.next).toHaveBeenCalled();
+      expect(ReloadPermissionsEvent.next).toHaveBeenCalled();
+    });
+  });
+});
