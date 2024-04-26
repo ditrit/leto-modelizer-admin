ff80514f4ac1b25cae6be7c76faa1ad29a7d5b1f
Justine Hell <hell.justine@gmail.com>
2024-04-26 10:16:13 +0200

    Update GroupPage to update Url query from tabs

--- src/pages/GroupPage.vue
+++ src/pages/GroupPage.vue
@@ -23,10 +23,11 @@
       </q-card-section>
       <q-card-section class="q-pa-none">
         <q-tabs
-          v-model="currentTab"
+          :model-value="currentTab"
           no-caps
           active-color="primary"
           align="left"
+          @update:model-value="updateUrl"
         >
           <q-tab
             name="users"
@@ -58,40 +59,45 @@
       />
     </q-card>
     <q-tab-panels
-      v-model="currentTab"
+      :model-value="currentTab"
       animated
       transition-prev="jump-up"
       transition-next="jump-down"
       class="bg-grey-1"
+      @update:model-value="updateUrl"
     >
       <users-tab-panel
         name="users"
         type="group"
         :entity="group"
+        @update:users-query="(v) => setTabsQuery('users', v)"
       />
       <access-control-tab-panel
         name="groups"
         type="group"
         sub-type="group"
         :entity="group"
+        @update:access-control-query="(v) => setTabsQuery('groups', v)"
       />
       <access-control-tab-panel
         name="roles"
         type="role"
         sub-type="group"
         :entity="group"
+        @update:access-control-query="(v) => setTabsQuery('roles', v)"
       />
       <permissions-tab-panel
         name="permissions"
         type="group"
         :entity="group"
+        @update:permissions-query="(v) => setTabsQuery('permissions', v)"
       />
     </q-tab-panels>
   </q-page>
 </template>
 
 <script setup>
-import { onMounted, ref } from 'vue';
+import { onMounted, ref, computed } from 'vue';
 import { useRoute, useRouter } from 'vue-router';
 import * as GroupService from 'src/services/GroupService';
 import { Notify } from 'quasar';
@@ -100,12 +106,39 @@ import UsersTabPanel from 'components/tab-panel/UsersTabPanel.vue';
 import AccessControlTabPanel from 'components/tab-panel/AccessControlTabPanel.vue';
 import PermissionsTabPanel from 'components/tab-panel/PermissionsTabPanel.vue';
 
-const loading = ref(false);
 const { t } = useI18n();
 const route = useRoute();
 const router = useRouter();
+const loading = ref(false);
 const group = ref({});
-const currentTab = ref('users');
+const tabsQuery = ref({
+  users: {},
+  groups: {},
+  roles: {},
+  permissions: {},
+});
+const currentTab = computed(() => route.query.tab || 'users');
+
+/**
+ * Update url with current tab query params.
+ * @param {string} tab - Tab Name.
+ */
+function updateUrl(tab) {
+  const query = { tab, ...tabsQuery.value[tab] };
+  const queryString = new URLSearchParams(query).toString();
+
+  router.push(`/groups/${group.value.id}?${queryString}`);
+}
+
+/**
+ * Set tabsQuery and call updateUrl.
+ * @param {string} tab - Tab name.
+ * @param {object} query - Query params object emitted from corresponding tab.
+ */
+function setTabsQuery(tab, query) {
+  tabsQuery.value[tab] = query;
+  updateUrl(tab);
+}
 
 /**
  * Load group from id in url. If the group does not exist, redirect to the libraries page.
